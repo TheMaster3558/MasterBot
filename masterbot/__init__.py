acde23644844dd3e41be4bfdcb3c68cb6292326c
chawkk6404 <chawk_k@outlook.com>
2022-02-13 10:40:47 -0800

    version info

--- masterbot/__init__.py
+++ masterbot/__init__.py
@@ -13,9 +13,22 @@ from .bot import MasterBot, DatabaseFolderNotFound
 from .cogs import cog_list
 from .api_keys import MasterBotAPIKeyManager
 
+from collections import namedtuple
+from typing import Literal
+
 
 __title__ = 'masterbot'
 __version__ = '1.0.0b'
 __author__ = 'The Master'
 __copyright__ = 'Copyright 2021-present The Master'
 __license__ = 'MIT'
+
+
+class VersionInfo(namedtuple):
+    major: int
+    minor: int
+    micro: int
+    release: Literal["a", "b", "rc", "final"]
+
+
+version_info = VersionInfo(major=1, minor=0, micro=0, release="a")
