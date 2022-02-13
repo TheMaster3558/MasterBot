c0d279ea9f7bb4256b842421aea90fe5888632f8
chawkk6404 <chawk_k@outlook.com>
2022-02-12 22:40:49 -0800

    button rework for readability

--- masterbot/cogs/trivia.py
+++ masterbot/cogs/trivia.py
@@ -34,86 +34,52 @@ class OpenTDBHTTPClient(AsyncHTTPClient):
         self.token = resp.get('token')
 
 
-def view(correct: str, wrong: list):
-    choices = wrong + [correct]
-    choices = [unescape(choice) for choice in choices]
-    random.shuffle(choices)
+colors = [discord.ButtonStyle.green, discord.ButtonStyle.red, discord.ButtonStyle.blurple, discord.ButtonStyle.grey]
 
-    class MultipleChoice(View):
-        def __init__(self):
-            self.tries = {}
-            self.done = False
-            super().__init__(timeout=30)
 
-        async def interaction_check(self, interaction: discord.Interaction) -> bool:
-            if self.done:
-                self.stop()
-                return False
-            if interaction.user.display_name in self.tries.keys():
-                await interaction.response.send_message('You already tried and failed.', ephemeral=True)
-                return False
-            return True
+class TriviaButton(discord.ui.Button['MultipleChoice']):
+    def __init__(self, choice, correct, color):
+        self.correct = correct
+        super().__init__(style=color, label=choice)
 
-        @discord.ui.button(style=discord.ButtonStyle.red, label=choices[0])
-        async def choice1(self, button: discord.ui.Button, interaction: discord.Interaction):
-            if button.label == correct:
-                self.done = True
-                await self.disable_all(message=interaction.message)
-                embed = discord.Embed(title=f'{interaction.user.display_name} got it right!',
-                                      description=f'The answer was {correct}')
-                if len(self.tries) > 0:
-                    embed.add_field(name='Wrong guesses', value='\n'.join(f'{k}: {v}' for k, v in self.tries.items()))
-                await interaction.response.send_message(embed=embed)
-                return self.stop()
-            await interaction.response.send_message('Wrong answer.', ephemeral=True)
-            self.tries[interaction.user.display_name] = button.label
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        if self.correct is True:
+            self.view.done = True
+            await self.view.disable_all(message=interaction.message)
+            embed = discord.Embed(title=f'{interaction.user.display_name} got it right!',
+                                  description=f'The answer was {self.label}')
+            if len(self.view.tries) > 0:
+                embed.add_field(name='Wrong guesses', value='\n'.join(f'{k}: {v}' for k, v in self.view.tries.items()))
+            await interaction.response.send_message(embed=embed)
+            return self.view.stop()
+        await interaction.response.send_message('Wrong answer.', ephemeral=True)
+        self.view.tries[interaction.user.display_name] = self.label
 
-        @discord.ui.button(style=discord.ButtonStyle.green, label=choices[1])
-        async def choice2(self, button: discord.ui.Button, interaction: discord.Interaction):
-            if button.label == correct:
-                self.done = True
-                await self.disable_all(message=interaction.message)
-                embed = discord.Embed(title=f'{interaction.user.display_name} got it right!',
-                                      description=f'The answer was {correct}')
-                if len(self.tries) > 0:
-                    embed.add_field(name='Wrong guesses', value='\n'.join(f'{k}: {v}' for k, v in self.tries.items()))
-                await interaction.response.send_message(embed=embed)
-                return self.stop()
-            await interaction.response.send_message('Wrong answer.', ephemeral=True)
-            self.tries[interaction.user.display_name] = button.label
 
-        if len(choices) > 2:
-            @discord.ui.button(style=discord.ButtonStyle.blurple, label=choices[2])
-            async def choice3(self, button: discord.ui.Button, interaction: discord.Interaction):
-                if button.label == correct:
-                    self.done = True
-                    await self.disable_all(message=interaction.message)
-                    embed = discord.Embed(title=f'{interaction.user.display_name} got it right!',
-                                          description=f'The answer was {correct}')
-                    if len(self.tries) > 0:
-                        embed.add_field(name='Wrong guesses',
-                                        value='\n'.join(f'{k}: {v}' for k, v in self.tries.items()))
-                    await interaction.response.send_message(embed=embed)
-                await interaction.response.send_message('Wrong answer.', ephemeral=True)
-                self.tries[interaction.user.display_name] = button.label
+class MultipleChoice(View):
+    def __init__(self, correct, incorrect: list):
+        self.incorrect = incorrect
+        self.choices: list = incorrect + [correct]
+        self.choices = [unescape(choice) for choice in self.choices]
+        random.shuffle(self.choices)
+        self.done = False
+        self.tries = {}
+        super().__init__(timeout=30)
+        for index, choice in enumerate(self.choices):
+            answer = False
+            if choice == correct:
+                answer = True
+            self.add_item(TriviaButton(choice, answer, colors[index]))
 
-        if len(choices) > 3:
-            @discord.ui.button(style=discord.ButtonStyle.gray, label=choices[3])
-            async def choice4(self, button: discord.ui.Button, interaction: discord.Interaction):
-                if button.label == correct:
-                    self.done = True
-                    await self.disable_all(message=interaction.message)
-                    embed = discord.Embed(title=f'{interaction.user.display_name} got it right!',
-                                          description=f'The answer was {correct}')
-                    if len(self.tries) > 0:
-                        embed.add_field(name='Wrong guesses',
-                                        value='\n'.join(f'{k}: {v}' for k, v in self.tries.items()))
-                    await interaction.response.send_message(embed=embed)
-                    return self.stop()
-                await interaction.response.send_message('Wrong answer.', ephemeral=True)
-                self.tries[interaction.user.display_name] = button.label
-
-    return MultipleChoice()
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if self.done:
+            self.stop()
+            return False
+        if interaction.user.display_name in self.tries.keys():
+            await interaction.response.send_message('You already tried and failed.', ephemeral=True)
+            return False
+        return True
 
 
 class Trivia(slash_util.Cog):
@@ -128,13 +94,12 @@ class Trivia(slash_util.Cog):
     async def trivia(self, ctx):
         data = await self.http.trivia()
         if data.get('response_code') != 0:
-            print(data)
             return await ctx.send('We encountered an unexpected error. Try again later.')
         data = data.get('results')[0]
         question = unescape(data.get('question'))
         embed = discord.Embed(title=question)
         embed.set_footer(text='The difficulty is {}'.format(data.get('difficulty')))
-        my_view = view(data.get('correct_answer'), data.get('incorrect_answers'))
+        my_view = MultipleChoice(data.get('correct_answer'), data.get('incorrect_answers'))
         message = await ctx.send(embed=embed, view=my_view)
         await my_view.wait()
         if my_view.done is False:
