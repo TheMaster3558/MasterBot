e74709afd2e1ca86daefdbaeb2f7bf16cb94dfff
chawkk6404 <chawk_k@outlook.com>
2022-03-05 16:20:13 -0800

    Snowflake -> int

--- cogs/games.py
+++ cogs/games.py
@@ -20,7 +20,7 @@ from datetime import time
 words = [word for word in english_words_lower_set if len(word) == 5]
 
 
-UserID = TypeVar('UserID', bound=Type[discord.abc.Snowflake])
+UserID = TypeVar('UserID', bound=Type[int])
 Num = Literal[1, 2, 3]
 
 
