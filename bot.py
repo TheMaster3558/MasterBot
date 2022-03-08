7d2b7fa6900fea6ef5a7db0cc73f06317b1238fd
chawkk6404 <chawk_k@outlook.com>
2022-03-07 16:05:15 -0800

    update version to 1.3.3

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ CogT = TypeVar('CogT', bound=commands.Cog)
 
 
 class MasterBot(commands.Bot):
-    __version__ = '1.3.2'
+    __version__ = '1.3.3'
     test_guild = discord.Object(id=878431847162466354)
 
     def __init__(self, cr_api_key: str, weather_api_key: str, mongo_db: str, /) -> None:
