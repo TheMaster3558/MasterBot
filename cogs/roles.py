39697af12bd9bf786926b64b1926a8a055321107
chawkk6404 <chawk_k@outlook.com>
2022-04-10 17:34:24 -0700

    Role selects

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,15 +1,140 @@
+from typing import Tuple, Optional, Iterable, Literal
+import asyncio
+
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 from bot import MasterBot
 from cogs.utils.app_and_cogs import Cog
+from cogs.utils.view import View
+
+
+MISSING = discord.utils.MISSING
+
+
+class RoleSelect(discord.ui.Select['RoleSelectView']):
+    def __init__(
+            self,
+            items,
+            *, mode: Literal[0, 1],
+    ):
+        options = [
+            discord.SelectOption(label=role.name,
+                                 emoji=emoji, value=str(role.id)) for role, emoji in items
+        ]
+
+        super().__init__(placeholder='Manage your roles', min_values=1, options=options, max_values=len(items))
+
+        self.mode = mode
+
+    async def callback(self, interaction: discord.Interaction):
+        roles = [interaction.guild.get_role(int(role_id)) for role_id in self.values]
+        if len(roles) == 0:
+            names = ['no roles']
+        else:
+            names = [role.name for role in roles]
+
+        word = 'Added' if self.mode == 0 else 'Removed'
+        func = discord.Member.add_roles if self.mode == 0 else discord.Member.remove_roles
+
+        await interaction.response.send_message(f'{word} {", ".join(names)}')
+        await func(interaction.user, *roles)  # type: ignore
+
+
+class RoleView(View):
+    def __init__(self,
+                 roles,
+                 emojis):
+        self.options = [(roles[i], emojis[i]) for i in range(len(roles) - 1)]
+
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label='Add', style=discord.ButtonStyle.green)
+    async def add(self, interaction: discord.Interaction, button: discord.ui.Button):
+        select = RoleSelect(self.options, mode=0)
+
+        view = View()
+        view.add_item(select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+
+    @discord.ui.button(label='Remove', style=discord.ButtonStyle.red)
+    async def remove(self, interaction: discord.Interaction, button: discord.ui.Button):
+        select = RoleSelect(self.options, mode=1)
+
+        view = View()
+        view.add_item(select)
+
+        await interaction.response.send_message(view=view, ephemeral=True)
+
+
+class RoleFlags(commands.FlagConverter):
+    channel: Optional[discord.TextChannel] = commands.CurrentChannel
+    roles: Tuple[discord.Role, ...]
+    emojis: Optional[Tuple[discord.Emoji | discord.PartialEmoji | str, ...]] = []
+    title: Optional[str] = None
+    description: Optional[str] = None
+    image: Optional[str] = None
+    color: Optional[discord.Color | int]
 
 
 class ReactionRoles(Cog, name='reactions'):
     def __init__(self, bot: MasterBot):
         super().__init__(bot)
         print('Roles cog loaded')
+
+    async def cog_command_error(self, ctx, error) -> None:
+        error: commands.CommandError
+
+        if isinstance(error, (commands.NoPrivateMessage, commands.MissingPermissions)):
+            return
+        await self.bot.on_command_error(ctx, error)
+
+    @commands.command(description='Set up a select menu for a similar version of "Reaction Roles"')
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    async def role(self, ctx: commands.Context, *, flags: RoleFlags):
+        if len(flags.roles) > 25:
+            msg = await ctx.send("You can't have more than 25 roles.")
+
+            await asyncio.sleep(5)
+            try:
+                await msg.delete()
+            except discord.NotFound:
+                pass
+            return
+
+        embed = discord.Embed(title=flags.title, description=flags.description)
+
+        if flags.image:
+            if not flags.image.startswith('http://') or not flags.image.startswith('https://'):
+                msg = await ctx.send(
+                    'The image must start with `http://` or `https://`'
+                )
+
+                await asyncio.sleep(5)
+                try:
+                    await msg.delete()
+                except discord.NotFound:
+                    pass
+                return
+            embed.set_image(url=flags.image)
+
+        flags.emojis = list(flags.emojis)
+        while len(flags.emojis) < len(flags.roles):
+            flags.emojis.append(None)  # type: ignore
+
+        while "None" in flags.emojis:
+            index = flags.emojis.index("None")
+            flags.emojis[index] = None  # type: ignore
+
+        view = RoleView(flags.roles, flags.emojis)
+
+        try:
+            await flags.channel.send(embed=embed, view=view)
+        except discord.Forbidden:
+            await ctx.send(f"I don't have permission to send messages in {flags.channel.mention}")
 
 
 async def setup(bot: MasterBot):
--- monkeypatches.py
+++ monkeypatches.py
@@ -5,7 +5,7 @@ class Embed(discord.Embed):
     """Set the default color to 0x2F3136"""
 
     def __init__(self, **kwargs):
-        if 'color' not in kwargs and 'colour' not in kwargs:
+        if not kwargs.get('color') and kwargs.get('colour'):
             kwargs['color'] = 0x2F3136
 
         super().__init__(**kwargs)
